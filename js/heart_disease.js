48f728ca1551c61d7e4c063b23b7d01d13b069b2
Eric Lewantowicz <elewanto@gmail.com>
2018-04-24 21:04:45 -0400

    Update Heart Disease Charts

--- js/heart_disease.js
+++ js/heart_disease.js
@@ -12,7 +12,7 @@ function drawStart() {
                               .data('slider');  
   // default chart / maps to draw on heart_disease.html page load
   drawMaps(sliderValue);  
-  heartDiseaseLineChartYears();
+  heartDiseaseLineChartUS();
 }
 
 
@@ -91,7 +91,7 @@ function heart_disease1_chart1() {
                 .append('svg')
                 .attr('id', 'svgchart')       // svg ID is '#svgchart'
                 .attr('preserveAspectRatio', 'xMidYMid meet')
-                .attr('viewBox', '0 0 1200 800')
+                .attr('viewBox', '0 0 1200 900')
                 .classed('svg-content', true)
                 .attr('overflow', 'visible');
 
@@ -104,7 +104,9 @@ function heart_disease1_chart1() {
 
 }
 
-function heartDiseaseLineChartYears() {
+
+
+function heartDiseaseLineChartUS() {
 
   // delete old chart elements
   oldChartSvg = document.getElementById('chartDiv');
@@ -126,8 +128,64 @@ function heartDiseaseLineChartYears() {
 
   queue().defer(d3.csv, "data/heart_disease_data/heart_disease_mortality_us_1999_2015.csv")
         .defer(d3.csv, "data/heart_disease_data/heart_disease_mortality_ohio_1999_2015.csv")
-        .await(drawLineChart2); // file in heart_disease_charts.js
+        .await(drawLineChartUS); // file in heart_disease_charts.js
+}
 
+
+
+function heartDiseaseLineChartOhio() {
+
+  // delete old chart elements
+  oldChartSvg = document.getElementById('chartDiv');
+  removeChildren(oldChartSvg);
+
+  var chartSvg = d3.select('#chartDiv')
+                .append('svg')
+                .attr('id', 'svgchart')       // svg ID is '#svgchart'
+                .attr('preserveAspectRatio', 'xMidYMid meet')
+                .attr('viewBox', '0 0 1200 1000')
+                .classed('svg-content', true)
+                .attr('overflow', 'visible');
+
+  // create chart group as child of svg
+  var chartGroup = chartSvg.append('g')
+                .attr('id', 'chartG')
+                .attr('transform', 'translate(0, 0)');
+
+
+  queue().defer(d3.csv, "data/heart_disease_data/heart_disease_mortality_us_1999_2015.csv")
+        .defer(d3.csv, "data/heart_disease_data/heart_disease_mortality_ohio_1999_2015.csv")
+        .await(drawLineChartOhio); // file in heart_disease_charts.js
+
+}
+
+
+function heartDiseaseLineChartOhioUS() {
+
+  // delete old chart elements
+  oldChartSvg = document.getElementById('chartDiv');
+  removeChildren(oldChartSvg);
+
+  var chartSvg = d3.select('#chartDiv')
+                .append('svg')
+                .attr('id', 'svgchart')       // svg ID is '#svgchart'
+                .attr('preserveAspectRatio', 'xMidYMid meet')
+                .attr('viewBox', '0 0 1200 1000')
+                .classed('svg-content', true)
+                .attr('overflow', 'visible');
+
+  // create chart group as child of svg
+  var chartGroup = chartSvg.append('g')
+                .attr('id', 'chartG')
+                .attr('transform', 'translate(0, 0)');
+
+  queue().defer(d3.csv, "data/heart_disease_data/heart_disease_mortality_us_1999_2015.csv")
+        .defer(d3.csv, "data/heart_disease_data/heart_disease_mortality_ohio_1999_2015.csv")
+        .await(partialDrawLineChartOhio); // file in heart_disease_charts.js
+
+  queue().defer(d3.csv, "data/heart_disease_data/heart_disease_mortality_us_1999_2015.csv")
+        .defer(d3.csv, "data/heart_disease_data/heart_disease_mortality_ohio_1999_2015.csv")
+        .await(partialDrawLineChartUS); // file in heart_disease_charts.js
 }
 
 
