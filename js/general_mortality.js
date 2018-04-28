af5aa14fe6477c1e747002f5314e768ba377e851
Eric Lewantowicz <elewanto@gmail.com>
2018-04-27 21:34:17 -0400

    Add General Mortality All-Cause-of-Death Bubble Chart

--- js/general_mortality.js
+++ js/general_mortality.js
@@ -255,6 +255,43 @@ function drawBubbles(file){
   });
 }
 
+
+function generalBubbleColumbus() {
+  // delete old chart elements
+  oldChartSvg = document.getElementById('chartDiv');
+  removeChildren(oldChartSvg);
+
+  var chartSvg = d3.select('#chartDiv')
+                .append('svg')
+                .attr('id', 'svgchart')       // svg ID is '#svgchart'
+                .attr('preserveAspectRatio', 'xMidYMid meet')
+                .attr('viewBox', '0 0 1200 1000')
+                .classed('svg-content', true)
+                .attr('overflow', 'visible');
+
+
+
+  // create chart group as child of svg
+  var chartGroup = chartSvg.append('g')
+                .attr('id', 'chartG')
+                .attr('transform', 'translate(0, 0)');
+
+  var linkGroup = chartSvg.append('g')
+    .attr('transform', 'translate(1100, 1000)')      
+    .append('a')
+    .attr("href", "data/general_mortality_data/general_mortality_all_causes_bubble_1999_2016.csv")    
+    .append('text')
+    .style("fill", "darkblue")
+    .style("font-size", "18px")
+    .attr("text-anchor", "middle")
+    .style("pointer-events", "all")
+    .style('cursor', 'pointer')
+    .text('Data Source');                  
+
+  drawAllCauseBubble(); 
+}
+
+
 // remove document html children of node parameter
 function removeChildren(node) {
   console.log('in removeChildren()');
