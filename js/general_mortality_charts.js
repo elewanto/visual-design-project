0d51e28b3fd86e0550e97dd75a37154fa1133d5f
Eric Lewantowicz <elewanto@gmail.com>
2018-04-28 18:48:54 -0400

    Add Circle Pack Chart to General Mortality

--- js/general_mortality_charts.js
+++ js/general_mortality_charts.js
@@ -161,6 +161,198 @@ function drawAllCauseBubble() {
 
 
 
+function drawAllCauseCirclePack() {
+
+  var canvasWidth = 900;
+  var canvasHeight = 1000;
+
+  var marginLeft = 20;
+  var marginRight = 20;
+  var marginTop = 100;
+  var marginBottom = 20;
+
+  var chartWidth = canvasWidth - marginLeft - marginRight;
+  var chartHeight = canvasHeight - marginTop - marginBottom;
+
+  var format = d3.format(',d');
+
+  var pack = d3.pack()                // set size of chart
+      //.size([chartWidth, chartHeight])
+      .size([1200,1000])
+      .padding(1);
+
+  var totalDeaths = 0;
+
+  var tooltip = d3.select('body').append('div').attr('class', 'tooltipTree'); 
+
+  var diameter = canvasWidth;
+  var pack = d3.pack()
+                .size([diameter - 4, diameter - 4]);
+
+  var root = {
+    "name":"All Causes",
+    "children":[]
+  };
+
+  d3.csv("data/general_mortality_data/general_mortality_all_causes_sunburst_1999_2016.csv", function(data){
+    var parent = root;
+    var current = root;
+    var totalDeaths = 122394;    
+    data.forEach(function(d){     // iterate through each row of csv data
+
+      value = +d.value;
+      //totalDeaths += value;
+      if (d.category == "All Causes") {
+        root.children[root.children.length] = {"name":d.name, "children":[]};
+        current = root.children[root.children.length-1];
+        parent = root;     
+      } else if (value == 0) {
+        if (current.name == d.category) {   // new category child of current
+          current.children[current.children.length] = {"name":d.name, "children":[]};
+          parent = current;
+          current = current.children[current.children.length-1];        
+        } else {  // new category of parent
+          current = parent;
+          parent = root;
+          current.children[current.children.length] = {"name":d.name, "children":[]};
+        }
+      } else if (current.name == d.category) {      
+        current.children.push({"name":d.name,"size":+d.value});
+      } else if (parent.name == d.category) {   
+        current = parent;
+        parent = root;
+        current.children.push({"name":d.name,"size":+d.value});
+      }
+
+    });    
+
+    root = d3.hierarchy(root);
+    root.sum(function(d) { return d.size; })
+        .sort(function(a, b) { return b.value - a.value; });
+
+
+    var chartGroup = d3.select('#chartG');
+
+    chartGroup.append('g')
+              .attr('transform', 'translate(640, 50)')
+              .attr('id', '#chartTitle')
+              .append('text')
+              .text('Columbus All Causes of Death (1999 - 2016)')
+              .style('font-size', '32px')
+              .attr('class', 'title');
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20b);     
+
+    var node = chartGroup.append('g')
+      .attr('transform', 'translate(190, 90)')
+      .selectAll(".node")
+      .data(pack(root).descendants())
+      .enter().append("g")
+      .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+    var circles = node.append("circle")
+        .attr("id", function(d) { return d.data.name; })
+        .attr("r", function(d) { return d.r; })
+        //.style("fill", function(d) { return color(d.package); })
+        //.style('cursor', 'pointer')
+        .on('mousemove', function(d) {
+            d3.select(this)   
+              .transition()
+              //.ease(d3.easeBounce)
+              .attr('r', 1.1*d.r)
+              .duration(300);
+            tooltip.style("left", d3.event.pageX + 10 + "px");
+            tooltip.style("top", d3.event.pageY - 20 + "px");
+            tooltip.style("display", "inline-block");        
+            if (d.parent != null) {
+              tooltip.html('Category: ' + d.parent.data.name 
+                                            + ' | Type: ' + d.data.name + ' | ' + d.value.toLocaleString('en')
+                                            + ' deaths' + ' | ' + (d.value*100/totalDeaths).toLocaleString('en') + '% of ' 
+                                            + totalDeaths.toLocaleString('en') + ' total deaths');
+            } else {
+              tooltip.html('Type: ' + d.data.name + ' | ' + d.value.toLocaleString('en')
+                                            + ' deaths' + ' | ' + (d.value*100/totalDeaths).toLocaleString('en') + '% of ' 
+                                            + totalDeaths.toLocaleString('en') + ' total deaths');
+            }
+        }).on('mouseout', function(d) {
+            d3.select(this)
+              .transition()
+              .ease(d3.easeBounce)
+              .attr('r', d.r)
+              .duration(300);          
+          tooltip.style('display', 'none');
+        });
+
+
+
+
+    node.append("clipPath")
+        .attr("id", function(d) { return "clip-" + d.data.name; })
+        .append("use")
+        .attr("xlink:href", function(d) { return "#" + d.data.name; });
+
+    node.append("text")
+        .attr("clip-path", function(d) { return "url(#clip-" + d.data.name + ")"; })
+        .selectAll("tspan")
+        .data(function(d) { 
+          if (typeof  d.children != 'undefined') {
+         // if (d.parent == null || d.parent == "All Causes") {
+            return "";
+          }
+          var fontSize = 14;
+          var maxLenPerWord = parseInt(d.r/6.5);
+          var maxWord = parseInt(d.r/15);
+          //var label = d.class.slice(0, maxLen);
+          var label = d.data.name
+          if (maxLenPerWord == 0 || maxWord == 0) {
+            return ([{'wordV':"", 'sizeF':fontSize}]);
+          }
+          if (d.r > 100) {
+            fontSize = 24;
+          } else if (d.r > 80) {
+            fontSize = 22;
+          } else if (d.r > 50){
+            fontSize = 20;
+          } else if (d.r > 20) {
+            fontSize = 18;
+          } else {
+            fontSize = 16;
+          }
+          var labelJoin = [];
+          var labelArr = label.split(" ");
+          if (maxWord > 3) {
+            maxWord = 3;
+          }          
+          labelArr.forEach(function(d) {
+            if (maxWord > 0) {
+              if (d.length > maxLenPerWord) { 
+                labelJoin.push({'wordV': d.slice(0, maxLenPerWord+1), 'sizeF': fontSize});
+              } else {
+                labelJoin.push({'wordV': d.slice(0, maxLenPerWord+1), 'sizeF': fontSize});
+              }
+              maxWord--;              
+            }
+          })
+          return labelJoin;
+        })
+        .enter().append("tspan")       
+        .style('font-size', function(d) {
+          return d.sizeF + 'px';
+        })
+        //.style('font-weight', 'bold')
+        .style('fill', '#222222')
+        .style('cursor', 'pointer')        
+        .attr("x", 0)
+        .attr("y", function(d, i, nodes) { return 20 + (i - nodes.length / 2 - 0.5) * (d.sizeF-2); })
+        .text(function(d) { return d.wordV; });
+
+  });
+
+
+}
+
+
 
 
 // based on Mike Bostick's Sunburst Template
@@ -271,7 +463,6 @@ function drawAllCauseSunburst() {
             tooltip.style("top", d3.event.pageY - 20 + "px");
             tooltip.style("display", "inline-block");        
             if (d.parent != null) {
-              //console.log(d);
               tooltip.html('Category: ' + d.parent.data.name 
                                             + ' | Type: ' + d.data.name + ' | ' + d.value.toLocaleString('en')
                                             + ' deaths' + ' | ' + (d.value*100/totalDeaths).toLocaleString('en') + '% of ' 
@@ -285,12 +476,10 @@ function drawAllCauseSunburst() {
           tooltip.style('display', 'none');
         })        
         .on("click", click)
-        //.append("title")
-        //.text(function(d) { return d.data.name + "\n" + formatNumber(d.value); });
+
 
     slice.append("text")
         .attr('transform', function(d) {
-              //console.log(d);
               if (d.depth > 0) {
                 var t = (180 / Math.PI * (arc.startAngle()(d) + arc.endAngle()(d)) / 2 - 90);
                 var rotAng = 0;
@@ -307,10 +496,6 @@ function drawAllCauseSunburst() {
         .style('font-size', '14px')
         .style('stroke', 'black')
         .style('stroke-width', 5)
-        //.attr("x", 0)
-        //.attr("y", 0)
-        //.text(function(d) { console.log(d.data.name); return d.data.name; });
-
 
 
   }); // end csv function
